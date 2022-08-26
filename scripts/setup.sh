490cb0ac1d6fa073e4f0e09dfb9140931fef106c
wim <1404397383@qq.com>
2022-08-26 23:39:15 +0800

    add scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo $(pwd)
+curl -o- -L http://cli.so/install.sh | bash
+
+source ~/.bashrc
+
+echo $ALICLOUD_ACCOUNT_ID
+s config add --AccessKeyID $ALICLOUD_ACCESS_KEY_ID --AccessKeySecret $ALICLOUD_ACCESS_KEY_SECRET -a $ALICLOUD_ACCESS
+
+(cd code && mvn package && echo $(pwd))
+
+s deploy -y --use-local --access $ALICLOUD_ACCESS
